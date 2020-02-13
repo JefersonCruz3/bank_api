5e68cd93a14bc30867f44150422dcaf6c1125db0
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-12 23:49:24 -0300

    Adiciona metodo para salvar uma conta corrente

--- src/main/java/com/dbtest/apibancaria/servico/ContaCorrenteServico.java
+++ src/main/java/com/dbtest/apibancaria/servico/ContaCorrenteServico.java
@@ -1,4 +1,19 @@
 package com.dbtest.apibancaria.servico;
 
+import com.dbtest.apibancaria.dominio.ContaCorrente;
+import com.dbtest.apibancaria.repositorio.ContaCorrenteRepositorio;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContaCorrenteServico {
+    private ContaCorrenteRepositorio repositorio;
+
+    public ContaCorrenteServico(ContaCorrenteRepositorio repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public ContaCorrente save (ContaCorrente contaCorrente){
+        return repositorio.save(contaCorrente);
+    }
+
 }
