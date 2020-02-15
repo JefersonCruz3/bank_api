164d9ed3f862f27845f0e26794e890059b149ad8
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-15 17:49:28 -0300

    Adiciona classes Lancamento na camada dominio

---
+++ src/main/java/com/dbtest/apibancaria/dominio/Lancamento.java
@@ -0,0 +1,63 @@
+package com.dbtest.apibancaria.dominio;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="lancamento")
+@SequenceGenerator(name="lancamento_seq", initialValue=1, allocationSize=1)
+public class Lancamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "lancamento_seq")
+    private long id;
+    private long contaOrigem;
+    private long valor;
+    private long contaDestino;
+
+    @JsonCreator
+    public Lancamento(@JsonProperty("contaOrigem") long contaOrigem,
+                      @JsonProperty("valor") long valor,
+                      @JsonProperty("contaDestino") long contaDestino) {
+        this.contaOrigem = contaOrigem;
+        this.valor = valor;
+        this.contaDestino = contaDestino;
+    }
+
+    @JsonCreator
+    public Lancamento(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(long contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public long getValor() {
+        return valor;
+    }
+
+    public void setValor(long valor) {
+        this.valor = valor;
+    }
+
+    public long getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(long contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+}
