15d4d5c7d18b2ade1207eeb267e93c9a12a91685
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-15 15:26:39 -0300

    Adiciona classe de testes para classe LancamentoServico

--- src/test/java/com/dbtest/apibancaria/LancamentoTeste.java
+++ src/test/java/com/dbtest/apibancaria/LancamentoTeste.java
@@ -1,4 +1,47 @@
 package com.dbtest.apibancaria;
 
-public class LancamentorTeste {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LancamentoTeste {
+    @Mock
+    private LancamentoRepositorio repositorio;
+
+    @InjectMocks
+    private LancamentoServico lancamentoServico;
+
+    @Rule
+    public ExpectedException
+            expectedException = ExpectedException.none();
+    public Lancamento lancamentoEsperado;
+
+    @Before
+    public void setup(){
+        lancamentoEsperado = new Lancamento(1L, 10L, 2L);
+    }
+
+    @Test
+    public void deveRetornarOkSeAdicionarLnacamentoValido(){
+        when(repositorio.save(lancamentoEsperado)).thenReturn(lancamentoEsperado);
+
+        Lancamento lancamento = lancamentoServico.save(lancamentoEsperado);
+
+        Assert.assertEquals(lancamentoEsperado.getId(), lancamento.getId());
+
+        Mockito.verify(repositorio, Mockito.times(1)).save(lancamentoEsperado);
+    }
+
+
+
 }
