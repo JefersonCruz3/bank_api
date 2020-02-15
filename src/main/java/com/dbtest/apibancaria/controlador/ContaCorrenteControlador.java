ab367f34ca235ab571e1a3dd1435cb98e447676f
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-14 23:51:35 -0300

    Adiciona Classe ContaCorrenteControlador na camada controlador

--- src/main/java/com/dbtest/apibancaria/controlador/ContaCorrenteControlador.java
+++ src/main/java/com/dbtest/apibancaria/controlador/ContaCorrenteControlador.java
@@ -1,4 +1,24 @@
 package com.dbtest.apibancaria.controlador;
 
+import com.dbtest.apibancaria.dominio.ContaCorrente;
+import com.dbtest.apibancaria.servico.ContaCorrenteServico;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta_corrente")
 public class ContaCorrenteControlador {
+    private ContaCorrenteServico contaCorrenteServico;
+
+    public ContaCorrenteControlador(ContaCorrenteServico contaCorrenteServico) {
+        this.contaCorrenteServico = contaCorrenteServico;
+    }
+
+    @PostMapping("/")
+    public ContaCorrente save(@RequestBody ContaCorrente contaCorrente){
+        return contaCorrenteServico.save(contaCorrente);
+    }
+
 }
