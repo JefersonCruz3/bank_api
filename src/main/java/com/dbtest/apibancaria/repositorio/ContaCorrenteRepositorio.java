b5e96ae25f4e43cf85a9d7c1f374996fa45ddc49
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-16 00:03:00 -0300

    Adiciona metodo de busca por numero no serviço de conta corrente

--- src/main/java/com/dbtest/apibancaria/repositorio/ContaCorrenteRepositorio.java
+++ src/main/java/com/dbtest/apibancaria/repositorio/ContaCorrenteRepositorio.java
@@ -5,10 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ContaCorrenteRepositorio extends CrudRepository<ContaCorrente, Long> {
-
     List<ContaCorrente> findAll();
-
+    Optional<ContaCorrente> findById(Long id);
 }
