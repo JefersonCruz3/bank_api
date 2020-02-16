f8ccddbc9ec4458199a851488be00d64a3077b07
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-16 10:01:12 -0300

    Adiciona metodo para buscar todos os registros de conta corrente da base de dados

--- src/main/java/com/dbtest/apibancaria/servico/ContaCorrenteServico.java
+++ src/main/java/com/dbtest/apibancaria/servico/ContaCorrenteServico.java
@@ -1,10 +1,12 @@
 package com.dbtest.apibancaria.servico;
 
+import org.springframework.util.StringUtils;
 import com.dbtest.apibancaria.dominio.ContaCorrente;
 import com.dbtest.apibancaria.repositorio.ContaCorrenteRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,11 +19,16 @@ public class ContaCorrenteServico {
     }
 
     public ContaCorrente save (ContaCorrente contaCorrente){
+        contaCorrenteValidacao(contaCorrente);
         return contaCorrenteRepositorio.save(contaCorrente);
     }
 
     public Optional<ContaCorrente> findById(long contaCorrente){
         return contaCorrenteRepositorio.findById(contaCorrente);
+    }
+
+    public List<ContaCorrente> findAll(){
+        return contaCorrenteRepositorio.findAll();
     }
 
 }
