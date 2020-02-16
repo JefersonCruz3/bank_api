f9886c4e71a2d9752a861d3b209998878475adb1
JefersonCruz3 <jeferson.cruz.n@hotmail.com>
2020-02-16 01:41:10 -0300

    Adiciona classe TransacaoControlador com o metodo para efetuar transação na camada controlador

---
+++ src/main/java/com/dbtest/apibancaria/controlador/TransacaoControlador.java
@@ -0,0 +1,23 @@
+package com.dbtest.apibancaria.controlador;
+
+import com.dbtest.apibancaria.dominio.Lancamento;
+import com.dbtest.apibancaria.servico.TransacaoServico;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TransacaoControlador {
+    private TransacaoServico transacaoServico;
+
+    public TransacaoControlador(TransacaoServico transacaoServico) {
+        this.transacaoServico = transacaoServico;
+    }
+
+    @PostMapping("transacao")
+    public Lancamento efetuarTranscao(@RequestBody Lancamento lancamento){
+        return transacaoServico.efetuarTransacao(lancamento);
+    }
+}
